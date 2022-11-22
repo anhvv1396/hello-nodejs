4afc72aa549b65578624482619182796febe9710
anhvv <anhvv13@gmail.com>
2022-11-22 09:13:18 +0700

    second commit

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ app.get('/contract', function(req, res) {
     res.send('Hello node');
 })
 app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000 update!');
 })
